3bb0948329841402b5d9a38861d06d7bdc5a46c6
yn03 <mlkn862n@gmail.com>
2022-11-28 14:40:17 +0000

    練習

---
+++ First.java
@@ -0,0 +1,12 @@
+/*  
+複数行コメント
+*/
+public class First {
+  // 単一行コメント　
+  public static void main (String[] args) {
+    System.out.println("RPG:スッキリ魔王征伐");
+    System.out.println("Ver.0.1 by 湊");
+    System.out.println("＜ただいま鋭意学習・制作中＞");
+    System.out.println("プログラムを終了します");
+  }
+}
