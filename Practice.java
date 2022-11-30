135d9c6a297653eb89758d191c7f5b88b4e8e54e
yn03 <mlkn862n@gmail.com>
2022-11-30 14:59:51 +0000

    練習問題1

---
+++ Practice.java
@@ -0,0 +1,17 @@
+//練習問題1-1
+public class Practice {
+  public static void main (String[] args) {
+    int a = 3; int b = 5;  //1行にまとめてOK
+    int c = a*b;
+    System.out.println("縦幅3横幅5の長方形の面積は、"+ c);
+  }
+}
+
+
+/*練習問題１−２
+    boolean result = true;
+    char Character = '駆';  //char 1つの文字
+    double pi = 3.14;  //double 普通の小数
+    long nomber = 3141592658539979L;  //long 大きな整数
+    String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた";  //文字の並び
+＊/
