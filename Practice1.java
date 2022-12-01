6c33ffbed926429de51e84db20b4696ff9e2e196
yn03 <mlkn862n@gmail.com>
2022-12-01 07:41:41 +0000

    型変換、命令の実行

--- Practice1.java
+++ Practice1.java
@@ -1,6 +1,6 @@
 public class Practice1 {
   public static void main (String[] args) {
-  System.out.println("ようこそ占いの館へ");
+  System.out.println("ようこそ占いの館へ！");
   System.out.println("あなたの名前を入力してください");
   String name = new java.util.Scanner(System.in).nextLine();
   System.out.println("あなたの年齢を入力してください");
