5040d853714fe7c69e4d0b30bfe1ec60339a88f6
yn03 <mlkn862n@gmail.com>
2022-12-05 15:27:08 +0000

    記号をランダムに表示

---
+++ Main3.java
@@ -0,0 +1,30 @@
+public class Main3 {
+  public static void main(String[] args) {
+    int[] seq = new int[10];
+
+    //ランダムに生成
+    for (int i = 0; i<seq.length; i++) {
+      seq[i] = new java.util.Random().nextInt(4);
+    }
+
+    for (int i = 0; i<seq.length; i++) {
+      switch (seq[i]) {
+        case 0:
+          System.out.print("A");
+          break;
+
+        case 1:
+          System.out.print("T");
+          break;
+
+        case 2:
+          System.out.print("G");
+          break;
+
+        case 3:
+          System.out.print("C");
+          break;
+      }
+    }
+  }
+}
