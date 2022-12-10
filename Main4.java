96493e6011a7f309a7fba47cc7e7096c318fee49
yn03 <mlkn862n@gmail.com>
2022-12-10 07:45:09 +0000

    メソッド

---
+++ Main4.java
@@ -0,0 +1,12 @@
+public class Main4 {
+  public static void methodA() {
+    System.out.println("methodA");
+    methodB();
+  }
+  public static void methodB() {
+    System.out.println("methodB");
+  }
+  public static void main(String[] args) {
+    methodA();
+  }
+}
