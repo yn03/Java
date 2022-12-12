01d18dc382a00d5940ea114b3d5800c30e4921f6
yn03 <mlkn862n@gmail.com>
2022-12-12 16:22:02 +0000

    引数の利用

--- Main5.java
+++ Main5.java
@@ -1,4 +1,12 @@
-public class Main4 {
-  public static void methodA() {
+public class Main5 {
+  public static void main(String[] args) {
     System.out.println("メソッドを呼び出します");
-　　hello("湊");
+    hello("湊");
+    hello("あすか");
+    hello("菅原");
+    System.out.println("メソッドの呼び出しが終わりました");
+  }
+  public static void hello(String name) {
+    System.out.println(name + "さん、こんにちは！");
+  }
+}
