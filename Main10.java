cbd73a6adef7a6762d5ba9d6fbb92110894b309f
yn03 <mlkn862n@gmail.com>
2022-12-26 16:24:33 +0000

    オーバーロード

--- Main10.java
+++ Main10.java
@@ -5,8 +5,8 @@ public class Main10 {
   public static int add(int x, int y, int z) {
     return x+y+z;
   }
-   public static void main(String[] args){
-     System.out.println("10+20="+ add(10,20));
-     System.out.println("10+20+30="+add(10,20,30));
+  public static void main(String[] args){
+    System.out.println("10+20="+ add(10,20));
+    System.out.println("10+20+30="+add(10,20,30));
   }
 }
