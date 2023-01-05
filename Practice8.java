cdbda94efa0b27fa448019b388764801dfcdf0a5
yn03 <mlkn862n@gmail.com>
2023-01-05 17:31:21 +0000

    練習

---
+++ Practice8.java
@@ -0,0 +1,10 @@
+public class Practice8 {
+  public static int add(int x, int y){
+    int ans = x+y;
+    return ans;
+  }
+  public static void main(String[] args) {
+    int ans = add(250,30);
+    System.out.println("250+30="+ans);
+  }
+}
