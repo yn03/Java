75bd017d95539394f398881e300aaccd88b81e72
yn03 <mlkn862n@gmail.com>
2023-01-11 16:44:53 +0000

    計算機プログラム

--- Practice10.java
+++ Practice10.java
@@ -1,14 +1,8 @@
 public class Practice10 {
   public static void main(String[] args) {
     int a = 10; int b = 2;
-    int total = tasu(a,b);
-    int delta = hiku(a,b);
+    int total = CalcLogic.tasu(a,b);
+    int delta = CalcLogic.hiku(a,b);
     System.out.println("足すと" + total + "引くと" + delta);
-  }
-  public static int tasu(int a, int b){
-    return(a + b);
-  }
-  public static int hiku(int a, int b){
-    return(a - b);
   }
 }
