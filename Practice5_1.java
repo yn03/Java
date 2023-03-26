6510487acc3368b94fd542428f1d61334df1a159
yn03 <mlkn862n@gmail.com>
2023-03-26 13:10:22 +0000

    練習

---
+++ Practice5_1.java
@@ -0,0 +1,16 @@
+public  class Practice5_1{
+  public static void main(String[] args){
+    introduceOneself();
+  }
+
+
+  public static void introduceOneself(){
+    String name = "犬";
+    int age = 15;
+    double height = 18.2;
+    char zodiac = '牛';
+    System.out.println(name + "です");
+    System.out.println("年齢は" + age + "歳です");
+    System.out.println("干支は" + zodiac + "です");
+  }
+}
