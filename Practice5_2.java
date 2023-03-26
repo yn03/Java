2d97f5f04f8537b90ec6bc996946f08594958a77
yn03 <mlkn862n@gmail.com>
2023-03-26 13:56:40 +0000

    練習

---
+++ Practice5_2.java
@@ -0,0 +1,14 @@
+public class Practice5_2{
+  public static void main(String[] args){
+    String title = "こんにゃく";
+    String address = "uso@anvks.com";
+    String text = "余ってるんです";
+  email(title,address,text);
+  }
+
+  public static void email(String title,String address,String text){
+    System.out.println(address + "に、以下のメールを送信しました");
+    System.out.println("件名:" + title);
+    System.out.println("本文" + text);
+  }
+}
